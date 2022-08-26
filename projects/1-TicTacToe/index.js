b4b20a8d3ed736633188aa83180b69d33a6c69bb
ZmijaZ <zmijaz2001@gmail.com>
2022-08-26 15:34:34 +0200

    Made the game work correctly, just need to need name input, and maybe AI

--- projects/1-TicTacToe/index.js
+++ projects/1-TicTacToe/index.js
@@ -1,3 +1,44 @@
-const body = document.querySelector('body');
+const h1 = document.querySelector("#h1");
 
-body.style.backgroundColor = 'blue';
+const divArray = [];
+
+import { GameBoard } from "./objects.js";
+
+let Table = GameBoard();
+
+const board = document.querySelector("#board");
+board.style.color = "green";
+
+let numberOfTiles = 9;
+let playerX = true;
+
+for (let i = 0; i < numberOfTiles; i++) {
+  const divi = document.createElement("div");
+  board.appendChild(divi);
+  divArray.push(divi);
+
+  divi.addEventListener("click", function () {
+    if (divi.textContent == "" && playerX && h1.textContent == "TicTacToe") {
+      divi.textContent = "X";
+      Table.tiles[i] = "X";
+      divi.style.color = "red";
+
+      if (Table.checkWinner("X") == 1) divi.style.filter = "blur(4px)";
+      playerX = !playerX;
+      console.log(i + " selected");
+    } else if (divi.textContent == "" && !playerX) {
+      divi.textContent = "O";
+      Table.tiles[i] = "O";
+      divi.style.color = "green";
+
+      if (Table.checkWinner("O") == 1) divi.style.filter = "blur(4px)";
+
+      playerX = !playerX;
+      console.log(i + " selected");
+    } else {
+      console.log(divi.textContent);
+    }
+  });
+}
+
+export { h1, board };
---
+++ projects/1-TicTacToe/objects.js
@@ -0,0 +1,48 @@
+import { h1, board } from "./index.js";
+
+function GameBoard() {
+  let winner = "";
+
+  const tiles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  function EmptyTiles(tiles) {
+    for (let x of tiles) if (x !== "O" && x !== "X") return true;
+
+    return false;
+  }
+
+  function checkWinner(player) {
+    if (
+      (tiles[0] === tiles[1] && tiles[1] === tiles[2]) ||
+      (tiles[3] === tiles[4] && tiles[4] === tiles[5]) ||
+      (tiles[6] === tiles[7] && tiles[7] === tiles[8]) ||
+      (tiles[0] === tiles[3] && tiles[3] === tiles[6]) ||
+      (tiles[1] === tiles[4] && tiles[4] === tiles[7]) ||
+      (tiles[2] === tiles[5] && tiles[5] === tiles[8]) ||
+      (tiles[0] === tiles[4] && tiles[4] === tiles[8]) ||
+      (tiles[2] === tiles[4] && tiles[4] === tiles[6])
+    ) {
+      winner = player;
+      console.log(player + " won !");
+      h1.textContent = player + " won !";
+
+      return 1;
+    } else if (!EmptyTiles(tiles)) {
+      winner = "tie";
+      console.log("TIE!");
+      h1.textContent = "TIE";
+
+      return 2;
+    } else {
+      console.log("Still no winners " + tiles);
+      return 0;
+    }
+  }
+
+  return {
+    checkWinner,
+    tiles,
+  };
+}
+
+export { GameBoard };
