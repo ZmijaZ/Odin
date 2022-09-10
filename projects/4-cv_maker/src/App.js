8382819747123aaccff227cc643585be8edea8d7
ZmijaZ <zmijaz2001@gmail.com>
2022-09-10 09:38:37 +0200

    deleted wrong files, prepared for the project correctly

--- projects/4-cv_maker/src/App.js
+++ projects/4-cv_maker/src/App.js
@@ -1,24 +1,8 @@
 
-import {Form} from './components/Form.js'
-import { JobForm } from './components/Job.js';
-import {PersonalForm} from './components/Personal.js'
-import {ContactForm} from './components/Contact'
-import {School, SchoolForm} from './components/School.js'
-import { SkillForm } from './components/Skill.js';
-import { StrengthForm } from './components/Strength.js';
-
 function App() {
   return (
     <div className="App">
-
-      <PersonalForm></PersonalForm>
-      <ContactForm></ContactForm>
-      <SchoolForm></SchoolForm>
-      <JobForm></JobForm>
-      <SkillForm></SkillForm>
-      <StrengthForm></StrengthForm>
-
-      {/* <Personal></Personal> */}
+      
     </div>
   );
 }
